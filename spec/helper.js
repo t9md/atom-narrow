159b40050d2a9540bab8e91fb5e4ee1bdffb2859
t9md <taqumd@gmail.com>
2017-06-12 23:29:17 +0900

    convert to js: spec/helper

--- spec/helper.js
+++ spec/helper.js
@@ -1,224 +1,291 @@
-_ = require 'underscore-plus'
-{inspect} = require 'util'
-Ui = require '../lib/ui'
-ProviderBase = require "../lib/provider/provider-base"
+const _ = require("underscore-plus")
+const {inspect} = require("util")
+const Ui = require("../lib/ui")
+const ProviderBase = require("../lib/provider/provider-base")
 
-startNarrow = (providerName, options) ->
-  ProviderBase.start(providerName, options).then(getNarrowForUi)
+function startNarrow(providerName, options) {
+  return ProviderBase.start(providerName, options).then(getNarrowForUi)
+}
 
-reopen = ->
-  ProviderBase.reopen()
+function reopen() {
+  return ProviderBase.reopen()
+}
 
-getNarrowForUi = (ui) ->
-  provider = ui.provider
-  props = {provider, ui}
-  ensureer = new Ensureer(ui, provider)
-  for propName in ['ensure', 'waitsForRefresh', 'waitsForConfirm', 'waitsForDestroy', 'waitsForPreview']
+function getNarrowForUi(ui) {
+  const {provider} = ui
+  const props = {provider, ui}
+  const ensureer = new Ensureer(ui, provider)
+  const propNames = [
+    "ensure",
+    "waitsForRefresh",
+    "waitsForConfirm",
+    "waitsForDestroy",
+    "waitsForPreview",
+  ]
+  for (let propName of propNames) {
     props[propName] = ensureer[propName]
-  props
+  }
+  return props
+}
 
-dispatchCommand = (target, commandName) ->
+function dispatchCommand(target, commandName) {
   atom.commands.dispatch(target, commandName)
+}
 
-dispatchEditorCommand = (commandName, editor=null) ->
-  editor ?= atom.workspace.getActiveTextEditor()
+function dispatchEditorCommand(commandName, editor = null) {
+  if (!editor) editor = atom.workspace.getActiveTextEditor()
   atom.commands.dispatch(editor.element, commandName)
+}
 
-ensureCursorPosition = (editor, position) ->
+function ensureCursorPosition(editor, position) {
   expect(editor.getCursorBufferPosition()).toEqual(position)
+}
 
-validateOptions = (options, validOptions, message) ->
-  invalidOptions = _.without(_.keys(options), validOptions...)
-  if invalidOptions.length
-    throw new Error("#{message}: #{inspect(invalidOptions)}")
+function validateOptions(options, validOptions, message) {
+  const invalidOptions = _.without(_.keys(options), ...validOptions)
+  if (invalidOptions.length) {
+    throw new Error(`${message}: ${inspect(invalidOptions)}`)
+  }
+}
 
-ensureEditor = (editor, options) ->
-  ensureEditorOptionsOrdered = [
-    'cursor', 'text', 'active', 'alive'
-  ]
-  validateOptions(options, ensureEditorOptionsOrdered, "invalid options ensureEditor")
-  for name in ensureEditorOptionsOrdered when (value = options[name])?
-    switch name
-      when 'cursor'
-        expect(editor.getCursorBufferPosition()).toEqual(value)
-      when 'active'
-        expect(atom.workspace.getActiveTextEditor() is editor).toBe(value)
-      when 'alive'
-        expect(editor.isAlive()).toBe(value)
+function ensureEditor(editor, options) {
+  let value
+  const ensureEditorOptionsOrdered = ["cursor", "text", "active", "alive"]
+  validateOptions(
+    options,
+    ensureEditorOptionsOrdered,
+    "invalid options ensureEditor"
+  )
+  for (const name of ensureEditorOptionsOrdered) {
+    const value = options[name]
+    if (value == null) continue
 
-ensureEditorIsActive = (editor) ->
+    if (name === "cursor") {
+      expect(editor.getCursorBufferPosition()).toEqual(value)
+    } else if (name === "active") {
+      expect(atom.workspace.getActiveTextEditor() === editor).toBe(value)
+    } else if (name === "alive") {
+      expect(editor.isAlive()).toBe(value)
+    }
+  }
+}
+
+function ensureEditorIsActive(editor) {
   expect(atom.workspace.getActiveTextEditor()).toBe(editor)
+}
 
-isProjectHeaderItem = (item) ->
-  item.header? and item.projectName and not item.filePath?
+function isProjectHeaderItem(item) {
+  item.header && item.projectName && !item.filePath
+}
 
-isFileHeaderItem = (item) ->
-  item.header? and item.filePath?
+function isFileHeaderItem(item) {
+  item.header && item.filePath
+}
 
-addCustomMatchers = (spec) ->
-  spec.addMatchers
-    toEqualSearchItems: (expected) ->
-      @message = ->
-        "Expected '" + inspect(@actual) + "' to equal '" + inspect(expected)
+const ensureOptionsOrdered = [
+  "itemsCount",
+  "selectedItemRow",
+  "selectedItemText",
+  "text",
+  "cursor",
+  "classListContains",
+  "filePathForProviderPane",
+  "query",
+  "searchItems",
+  "columnForSelectedItem",
+]
+// var Ensureer = (function() {
+// let ensureOptionsOrdered = undefined
+class Ensureer {
+  constructor(ui, provider) {
+    this.waitsForDestroy = this.waitsForDestroy.bind(this)
+    this.waitsForRefresh = this.waitsForRefresh.bind(this)
+    this.waitsForConfirm = this.waitsForConfirm.bind(this)
+    this.waitsForPreview = this.waitsForPreview.bind(this)
+    this.ensure = this.ensure.bind(this)
 
-      _.isEqual(@actual, expected)
+    this.ui = ui
+    this.provider = provider
+    this.editor = this.ui.editor
+    this.items = this.ui.items
+    this.editorElement = this.ui.editorElement
+  }
 
-beforeEach ->
-  addCustomMatchers(this)
-
-class Ensureer
-  constructor: (@ui, @provider) ->
-    {@editor, @items, @editorElement} = @ui
-
-  ensureOptionsOrdered = [
-    'itemsCount', 'selectedItemRow', 'selectedItemText'
-    'text', 'cursor', 'classListContains'
-    'filePathForProviderPane'
-    'query'
-    'searchItems'
-    'columnForSelectedItem'
-  ]
-
-  waitsForDestroy: (fn) =>
-    disposable = @ui.onDidDestroy -> disposable.dispose()
+  waitsForDestroy(fn) {
+    const disposable = this.ui.onDidDestroy(() => disposable.dispose())
     fn()
-    waitsFor -> disposable.disposed
+    waitsFor(() => disposable.disposed)
+  }
 
-  waitsForRefresh: (fn) =>
-    disposable = @ui.onDidRefresh -> disposable.dispose()
+  waitsForRefresh(fn) {
+    const disposable = this.ui.onDidRefresh(() => disposable.dispose())
     fn()
-    waitsFor -> disposable.disposed
+    waitsFor(() => disposable.disposed)
+  }
 
-  waitsForConfirm: (fn) =>
-    disposable = @ui.onDidConfirm -> disposable.dispose()
+  waitsForConfirm(fn) {
+    const disposable = this.ui.onDidConfirm(() => disposable.dispose())
     fn()
-    waitsFor -> disposable.disposed
+    waitsFor(() => disposable.disposed)
+  }
 
-  waitsForPreview: (fn) =>
-    disposable = @ui.onDidPreview -> disposable.dispose()
+  waitsForPreview(fn) {
+    const disposable = this.ui.onDidPreview(() => disposable.dispose())
     fn()
-    waitsFor -> disposable.disposed
+    waitsFor(() => disposable.disposed)
+  }
 
-  ensure: (args...) =>
-    switch args.length
-      when 1 then [options] = args
-      when 2 then [query, options] = args
+  ensure(...args) {
+    let options, query
+    if (args.length === 1) {
+      ;[options] = args
+    } else if (args.length === 2) {
+      ;[query, options] = args
+    }
 
-    validateOptions(options, ensureOptionsOrdered, 'Invalid ensure option')
+    validateOptions(options, ensureOptionsOrdered, "Invalid ensure option")
 
-    ensureOptions = =>
-      for name in ensureOptionsOrdered when options[name]?
-        method = 'ensure' + _.capitalize(_.camelize(name))
-        this[method](options[name])
+    const ensureOptions = () => {
+      for (let name of ensureOptionsOrdered) {
+        if (options[name] != null) {
+          const method = `ensure${_.capitalize(_.camelize(name))}`
+          this[method](options[name])
+        }
+      }
+    }
 
-    if query?
-      runs =>
-        @waitsForRefresh =>
-          @ui.setQuery(query)
-          if @ui.autoPreviewOnQueryChange
-            advanceClock(200)
-          @ui.moveToPrompt()
-      runs -> ensureOptions()
-    else
+    if (query) {
+      runs(() => {
+        this.waitsForRefresh(() => {
+          this.ui.setQuery(query)
+          if (this.ui.autoPreviewOnQueryChange) advanceClock(200)
+          this.ui.moveToPrompt()
+        })
+      })
+      runs(() => ensureOptions())
+    } else {
       ensureOptions()
+    }
+  }
 
-  ensureItemsCount: (count) ->
-    expect(@items.getCount()).toBe(count)
+  ensureItemsCount(count) {
+    expect(this.items.getCount()).toBe(count)
+  }
 
-  ensureSelectedItemRow: (row) ->
-    expect(@items.getRowForSelectedItem()).toBe(row)
+  ensureSelectedItemRow(row) {
+    expect(this.items.getRowForSelectedItem()).toBe(row)
+  }
 
-  ensureSelectedItemText: (text) ->
-    expect(@items.getSelectedItem().text).toBe(text)
+  ensureSelectedItemText(text) {
+    expect(this.items.getSelectedItem().text).toBe(text)
+  }
 
-  ensureText: (text) ->
-    expect(@editor.getText()).toBe(text)
+  ensureText(text) {
+    expect(this.editor.getText()).toBe(text)
+  }
 
-  ensureQuery: (text) ->
-    expect(@ui.getQuery()).toBe(text)
+  ensureQuery(text) {
+    expect(this.ui.getQuery()).toBe(text)
+  }
 
-  ensureSearchItems: (object) ->
-    relativizedFilePath = (item) ->
-      atom.project.relativize(item.filePath)
+  ensureSearchItems(object) {
+    const relativizedFilePath = item => atom.project.relativize(item.filePath)
 
-    actualObject = {}
-    projectName = null
-    for item in @ui.items.items[1...]
-      switch
-        when isProjectHeaderItem(item)
-          projectName = item.projectName
-          actualObject[projectName] = {}
-        when isFileHeaderItem(item)
-          actualObject[projectName][relativizedFilePath(item)] = []
-        else
-          itemText = @ui.getTextForItem(item)
-          actualObject[projectName][relativizedFilePath(item)].push(itemText)
+    const actualObject = {}
+    let projectName = null
+    for (let item of this.ui.items.items.slice(1)) {
+      if (isProjectHeaderItem(item)) {
+        projectName = item.projectName
+        actualObject[projectName] = {}
+      } else if (isFileHeaderItem(item)) {
+        actualObject[projectName][relativizedFilePath(item)] = []
+      } else {
+        const itemText = this.ui.getTextForItem(item)
+        actualObject[projectName][relativizedFilePath(item)].push(itemText)
+      }
+    }
 
     expect(actualObject).toEqual(object)
+  }
 
-  ensureCursor: (cursor) ->
-    expect(@editor.getCursorBufferPosition()).toEqual(cursor)
+  ensureCursor(cursor) {
+    expect(this.editor.getCursorBufferPosition()).toEqual(cursor)
+  }
 
-  ensureColumnForSelectedItem: (column) ->
-    cursorPosition = @editor.getCursorBufferPosition()
-    expect(@items.getRowForSelectedItem()).toBe(cursorPosition.row)
+  ensureColumnForSelectedItem(column) {
+    const cursorPosition = this.editor.getCursorBufferPosition()
+    expect(this.items.getRowForSelectedItem()).toBe(cursorPosition.row)
     expect(cursorPosition.column).toBe(column)
+  }
 
-  ensureClassListContains: (classList) ->
-    for className in classList
-      expect(@editorElement.classList.contains(className)).toBe(true)
+  ensureClassListContains(classList) {
+    for (const className of classList) {
+      expect(this.editorElement.classList.contains(className)).toBe(true)
+    }
+  }
 
-  ensureFilePathForProviderPane: (filePath) ->
-    result = @provider.getPane().getActiveItem().getPath()
+  ensureFilePathForProviderPane(filePath) {
+    const result = this.provider.getPane().getActiveItem().getPath()
     expect(result).toBe(filePath)
+  }
+}
 
-# example-usage
-# ensurePaneLayout
-#   horizontal: [
-#     [e1]
-#     vertical: [[e4], [e2, e3]]
-#   ]
-ensurePaneLayout = (layout) ->
-  root = atom.workspace.getActivePane().getContainer().getRoot()
+// example-usage
+// ensurePaneLayout
+//   horizontal: [
+//     [e1]
+//     vertical: [[e4], [e2, e3]]
+//   ]
+function ensurePaneLayout(layout) {
+  const root = atom.workspace.getActivePane().getContainer().getRoot()
   expect(paneLayoutFor(root)).toEqual(layout)
+}
 
-paneLayoutFor = (root) ->
-  switch root.constructor.name
-    when "Pane"
-      root.getItems()
-    when "PaneAxis"
-      layout = {}
+function paneLayoutFor(root) {
+  const name = root.constructor.name
+  switch (root.constructor.name) {
+    case "Pane":
+      return root.getItems()
+    case "PaneAxis":
+      const layout = {}
       layout[root.getOrientation()] = root.getChildren().map(paneLayoutFor)
-      layout
+      return layout
+  }
+}
 
-paneForItem = (item) ->
-  atom.workspace.paneForItem(item)
+function paneForItem(item) {
+  return atom.workspace.paneForItem(item)
+}
 
-setActiveTextEditor = (editor) ->
-  pane = paneForItem(editor)
+function setActiveTextEditor(editor) {
+  const pane = paneForItem(editor)
   pane.activate()
   pane.activateItem(editor)
+}
 
-setActiveTextEditorWithWaits = (editor) ->
-  runs ->
-    disposable = atom.workspace.onDidStopChangingActivePaneItem (item) ->
-      # This guard is necessary(only in spec), to ignore `undefined` item are passed.
-      if item is editor
-        disposable.dispose()
+function setActiveTextEditorWithWaits(editor) {
+  runs(() => {
+    let disposable
+    disposable = atom.workspace.onDidStopChangingActivePaneItem(item => {
+      // This guard is necessary(only in spec), to ignore `undefined` item are passed.
+      if (item === editor) disposable.dispose()
+    })
     setActiveTextEditor(editor)
-    waitsFor -> disposable.disposed
+    waitsFor(() => disposable.disposed)
+  })
+}
 
 module.exports = {
-  startNarrow
-  dispatchCommand
-  ensureCursorPosition
-  ensureEditor
-  ensurePaneLayout
-  ensureEditorIsActive
-  dispatchEditorCommand
-  paneForItem
-  setActiveTextEditor
-  setActiveTextEditorWithWaits
-  getNarrowForUi
-  reopen
+  startNarrow,
+  dispatchCommand,
+  ensureCursorPosition,
+  ensureEditor,
+  ensurePaneLayout,
+  ensureEditorIsActive,
+  dispatchEditorCommand,
+  paneForItem,
+  setActiveTextEditor,
+  setActiveTextEditorWithWaits,
+  getNarrowForUi,
+  reopen,
 }
