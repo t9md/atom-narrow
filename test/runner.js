cf656f7217490c8578296cfe5eb127e27f649a4d
t9md <taqumd@gmail.com>
2018-03-08 22:55:03 +0900

    :fire: comment

--- test/runner.js
+++ test/runner.js
@@ -7,21 +7,11 @@ module.exports = createRunner(
   {
     globalAtom: false,
     htmlTitle: `Narrow Package Tests - pid ${process.pid}`
-    // onCreated: () => {
-    //   // console.log('CREATED!!!')
-    //   // workspaceElement = atom.views.getView(atom.workspace)
-    //   // document.body.appendChild(workspaceElement)
-    //   // document.body.focus()
-    // }
   },
   mocha => {
     global.atom = global.buildAtomEnvironment({
       enablePersistence: false
     })
-    // mocha.ui('tdd')
-    // atom.applicationDelegate.setRepresentedFilename = () => {}
-    // atom.applicationDelegate.setWindowDocumentEdited = () => {}
-
     let packageName = require('../package.json').name
     const packageDir = path.join(atom.configDirPath, 'packages')
     fs.ensureSymlinkSync(path.dirname(__dirname), path.join(packageDir, packageName))
